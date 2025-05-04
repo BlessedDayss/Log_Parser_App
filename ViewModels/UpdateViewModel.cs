407c59bbba4cdee73caa8bb751fcabdd611f8658
o.gojaev <o.gojayev2000@gmail.com>
2025-05-04 02:39:52 +0200

    feat: update workflow references and enhance app version display in UI

--- ViewModels/UpdateViewModel.cs
+++ ViewModels/UpdateViewModel.cs
@@ -113,7 +113,7 @@ namespace Log_Parser_App.ViewModels
             {
                 AvailableUpdate = await _updateService.CheckForUpdatesAsync();
                 
-                if (AvailableUpdate != null && AvailableUpdate.Version != null && AvailableUpdate.Version != DefaultVersion)
+                if (AvailableUpdate != null && AvailableUpdate.Version != new Version(0, 0, 0))
                 {
                     StatusMessage = $"Доступно обновление: {AvailableUpdate.Version}";
                 }
