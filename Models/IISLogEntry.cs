0c8695bc7809f6067000c21fd61e3a21df09787b
OG <og>
2025-06-05 15:20:11 +0200

    Refactor IisLogEntry class structure and improve ShortUserAgent property implementation

--- Models/IISLogEntry.cs
+++ Models/IISLogEntry.cs
@@ -2,40 +2,76 @@ using System;
 
 namespace Log_Parser_App.Models
 {
-    public class IisLogEntry
-    { public DateTimeOffset? DateTime { get; set; } // Combined Date and Time (Typically UTC)
-        public string? ClientIPAddress { get; set; }   // c-ip
-        public string? UserName { get; set; }          // cs-username
-        public string? ServiceName { get; set; }       // s-sitename
-        public string? ServerName { get; set; }        // s-computername
-        public string? ServerIPAddress { get; set; }   // s-ip
-        public int? ServerPort { get; set; }           // s-port
-        public string? Method { get; set; }            // cs-method
-        public string? UriStem { get; set; }           // cs-uri-stem
-        public string? UriQuery { get; set; }          // cs-uri-query
-        public int? HttpStatus { get; set; }          // sc-status
-        public int? Win32Status { get; set; }         // sc-win32-status
-        public long? BytesSent { get; set; }           // sc-bytes
-        public long? BytesReceived { get; set; }       // cs-bytes
-        public int? TimeTaken { get; set; }           // time-taken (milliseconds)
-        public string? ProtocolVersion { get; set; }   // cs-version
-        public string? Host { get; set; }              // cs-host
-        public string? UserAgent { get; set; }         // cs(User-Agent)
-        public string? Cookie { get; set; }            // cs(Cookie)
-        public string? RawLine { get; set; }
 
-        private const int MaxUserAgentDisplayLength = 50; 
+	#region Class: IisLogEntry
 
-        public string? ShortUserAgent 
-        {
-            get
-            {
-                if (string.IsNullOrEmpty(UserAgent))
-                    return UserAgent; 
-                return UserAgent.Length <= MaxUserAgentDisplayLength
-                    ? UserAgent
-                    : UserAgent.Substring(0, MaxUserAgentDisplayLength) + "...";
-            }
-        }
-    }
-} 
+	public class IisLogEntry
+	{ 
+
+		#region Properties: Public
+
+		public DateTimeOffset? DateTime { get; set; }
+
+		public string? ClientIPAddress { get; set; }
+
+		public string? UserName { get; set; }
+
+		public string? ServiceName { get; set; }
+
+		public string? ServerName { get; set; }
+
+		public string? ServerIPAddress { get; set; }
+
+		public int? ServerPort { get; set; }
+
+		public string? Method { get; set; }
+
+		public string? UriStem { get; set; }
+
+		public string? UriQuery { get; set; }
+
+		public int? HttpStatus { get; set; }
+
+		public int? Win32Status { get; set; }
+
+		public long? BytesSent { get; set; }
+
+		public long? BytesReceived { get; set; }
+
+		public int? TimeTaken { get; set; }
+
+		public string? ProtocolVersion { get; set; }
+
+		public string? Host { get; set; }
+
+		public string? UserAgent { get; set; }
+
+		public string? Cookie { get; set; }
+
+		public string? RawLine { get; set; }
+
+		public string? ShortUserAgent 
+		{
+			get
+			{
+				if (string.IsNullOrEmpty(UserAgent))
+					return UserAgent; 
+				return UserAgent.Length <= MaxUserAgentDisplayLength
+					? UserAgent
+					: string.Concat(UserAgent.AsSpan(0, MaxUserAgentDisplayLength), "...");
+			}
+		}
+
+		#endregion
+
+		#region Constants: Private
+
+		private const int MaxUserAgentDisplayLength = 50;
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
