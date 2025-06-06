76f3a9bc550fc294dd846a99d23e70a5fa153160
OG <og>
2025-06-06 09:01:00 +0200

    Add SplashScreen and EmptyStateView components with initial layout and functionality

---
+++ Views/EmptyStateView.axaml.cs
@@ -0,0 +1,26 @@
+using Avalonia;
+using Avalonia.Controls;
+using Avalonia.Markup.Xaml;
+using Avalonia.Interactivity;
+using System;
+
+namespace Log_Parser_App.Views
+{
+    public partial class EmptyStateView : UserControl
+    {
+        public event EventHandler OpenLogFileRequested;
+        
+        public EmptyStateView()
+        {
+            InitializeComponent();
+            
+            var openLogFileButton = this.FindControl<Button>("OpenLogFileButton");
+            openLogFileButton.Click += (s, e) => OpenLogFileRequested?.Invoke(this, EventArgs.Empty);
+        }
+
+        private void InitializeComponent()
+        {
+            AvaloniaXamlLoader.Load(this);
+        }
+    }
+} 
