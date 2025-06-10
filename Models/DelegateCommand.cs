3f459157e51f36f912047855bb9b9e801e517a8e
OG <o.gojayev2000@gmail.com>
2025-06-10 16:37:06 +0200

    Refactor `DelegateCommand`: simplify constructor, reduce field usage, and streamline method implementation.

--- Models/DelegateCommand.cs
+++ Models/DelegateCommand.cs
@@ -1,20 +1,15 @@
-using System;
-using System.Windows.Input;
-
 namespace Log_Parser_App.Models
 {
-    public class DelegateCommand : ICommand
+    using System;
+    using System.Windows.Input;
+
+
+    public class DelegateCommand(Action execute, Func<bool>? canExecute = null) : ICommand
     {
-        private readonly Action _execute;
-        private readonly Func<bool>? _canExecute;
-        public DelegateCommand(Action execute, Func<bool>? canExecute = null)
-        {
-            _execute = execute;
-            _canExecute = canExecute;
-        }
-        public bool CanExecute(object? parameter) => _canExecute?.Invoke() ?? true;
-        public void Execute(object? parameter) => _execute();
+
+        public bool CanExecute(object? parameter) => canExecute?.Invoke() ?? true;
+        public void Execute(object? parameter) => execute();
         public event EventHandler? CanExecuteChanged;
         public void RaiseCanExecuteChanged() => CanExecuteChanged?.Invoke(this, EventArgs.Empty);
     }
-} 
+}
