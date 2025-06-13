e339de2b0cc70ef23d218d1002f96385716b159a
OG <o.gojayev2000@gmail.com>
2025-06-13 13:44:26 +0200

    Refactor `IIISLogParserService`: reorganize using directives, structure code with regions for clarity, and improve formatting consistency.

--- Models/Interfaces/IIISLogParserService.cs
+++ Models/Interfaces/IIISLogParserService.cs
@@ -1,12 +1,22 @@
 namespace Log_Parser_App.Models.Interfaces
 {
-    using Log_Parser_App.Models;
-    using System.Collections.Generic;
-    using System.Threading;
+using Log_Parser_App.Models;
+using System.Collections.Generic;
+using System.Threading;
 
+	#region Interface: IIISLogParserService
 
-    public interface IIISLogParserService
-    {
-        IAsyncEnumerable<IisLogEntry> ParseLogFileAsync(string filePath, CancellationToken cancellationToken);
-    }
+	public interface IIISLogParserService
+	{
+
+		#region Methods: Public
+
+		IAsyncEnumerable<IisLogEntry> ParseLogFileAsync(string filePath, CancellationToken cancellationToken);
+
+		#endregion
+
+	}
+
+	#endregion
+
 }
