434cdf9c7c390a5bf1433e2135fa866ebf4b2617
OG <o.gojayev2000@gmail.com>
2025-06-13 13:43:52 +0200

    Refactor `IFilePickerService`: reorganize using directives, structure code with regions for clarity, and improve formatting consistency.

--- Models/Interfaces/IFilePickerService.cs
+++ Models/Interfaces/IFilePickerService.cs
@@ -1,14 +1,26 @@
 namespace Log_Parser_App.Models.Interfaces
 {
-    using System.Collections.Generic;
-    using System.Threading.Tasks;
-    using Avalonia.Controls;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using Avalonia.Controls;
 
+	#region Interface: IFilePickerService
 
-    public interface IFilePickerService
-    {
-        Task<IEnumerable<string>> PickFilesAsync(Window? window);
-        Task<string?> PickDirectoryAsync(Window? window);
-        Task<(IEnumerable<string>? Files, string? Directory)> ShowFilePickerContextMenuAsync(Window? window);
-    }
+	public interface IFilePickerService
+	{
+
+		#region Methods: Public
+
+		Task<IEnumerable<string>> PickFilesAsync(Window? window);
+
+		Task<string?> PickDirectoryAsync(Window? window);
+
+		Task<(IEnumerable<string>? Files, string? Directory)> ShowFilePickerContextMenuAsync(Window? window);
+
+		#endregion
+
+	}
+
+	#endregion
+
 }
