947eec833dccd92092a7807e0b0bd2aaf72daf9f
OG <o.gojayev2000@gmail.com>
2025-06-13 13:40:08 +0200

    Refactor `BoolToTabColorConverter`: add regions for clarity, reorganize using directives, and improve formatting consistency.

--- Converters/BoolToTabColorConverter.cs
+++ Converters/BoolToTabColorConverter.cs
@@ -1,22 +1,32 @@
 namespace Log_Parser_App.Converters
 {
-    using System;
-    using System.Globalization;
-    using Avalonia.Data.Converters;
-    using Avalonia.Media;
+	using System;
+	using System.Globalization;
+	using Avalonia.Data.Converters;
+	using Avalonia.Media;
 
+	#region Class: BoolToTabColorConverter
 
-    public class BoolToTabColorConverter : IValueConverter
-    {
-        public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-            if (value is bool isSelected && isSelected) {
-                return new SolidColorBrush(Color.Parse("#3A3B3E"));
-            }
-            return new SolidColorBrush(Color.Parse("#2A2B2D"));
-        }
+	public class BoolToTabColorConverter : IValueConverter
+	{
 
-        public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
-            throw new NotImplementedException();
-        }
-    }
+		#region Methods: Public
+
+		public object Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+			if (value is bool isSelected && isSelected) {
+				return new SolidColorBrush(Color.Parse("#3A3B3E"));
+			}
+			return new SolidColorBrush(Color.Parse("#2A2B2D"));
+		}
+
+		public object ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
 }
