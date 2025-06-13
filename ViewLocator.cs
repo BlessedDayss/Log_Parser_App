82234071676e1cdd2fef87916c4660bca7de9890
OG <o.gojayev2000@gmail.com>
2025-06-13 13:23:49 +0200

    Refactor `ViewLocator`: reorganize code with regions for clarity and adjust formatting for consistency.

--- ViewLocator.cs
+++ ViewLocator.cs
@@ -1,31 +1,36 @@
-using System;
-using Avalonia.Controls;
-using Avalonia.Controls.Templates;
-using Log_Parser_App.ViewModels;
-
-namespace Log_Parser_App;
-
-public class ViewLocator : IDataTemplate
+namespace Log_Parser_App
 {
+	using System;
+	using Avalonia.Controls;
+	using Avalonia.Controls.Templates;
+	using Log_Parser_App.ViewModels;
 
-    public Control? Build(object? param)
-    {
-        if (param is null)
-            return null;
-        
-        var name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
-        var type = Type.GetType(name);
+	#region Class: ViewLocator
 
-        if (type != null)
-        {
-            return (Control)Activator.CreateInstance(type)!;
-        }
-        
-        return new TextBlock { Text = "Not Found: " + name };
-    }
+	public class ViewLocator : IDataTemplate
+	{
 
-    public bool Match(object? data)
-    {
-        return data is ViewModelBase;
-    }
+		#region Methods: Public
+
+		public Control? Build(object? param) {
+			if (param is null)
+				return null;
+			string name = param.GetType().FullName!.Replace("ViewModel", "View", StringComparison.Ordinal);
+			var type = Type.GetType(name);
+			if (type != null) {
+				return (Control)Activator.CreateInstance(type)!;
+			}
+			return new TextBlock { Text = "Not Found: " + name };
+		}
+
+		public bool Match(object? data) {
+			return data is ViewModelBase;
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
 }
