950ab9d978c45ba84b96e20bec129d0b20d736eb
OG <o.gojayev2000@gmail.com>
2025-06-13 13:40:43 +0200

    Refactor `LogLevelToBrushConverter`: reorganize using directives, structure code with regions for clarity, and improve formatting consistency.

--- Converters/LogLevelToBrushConverter.cs
+++ Converters/LogLevelToBrushConverter.cs
@@ -1,32 +1,42 @@
 namespace Log_Parser_App.Converters
 {
-    using System;
-    using System.Globalization;
-    using Avalonia.Data.Converters;
-    using Avalonia.Media;
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Avalonia.Media;
 
+	#region Class: LogLevelToBrushConverter
 
-    public class LogLevelToBrushConverter : IValueConverter
-    {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-            if (value is string levelString) {
-                return levelString.ToUpperInvariant() switch {
-                    "ERROR" => new SolidColorBrush(Colors.Red),
-                    "WARNING" => new SolidColorBrush(Colors.Orange),
-                    "INFO" => new SolidColorBrush(Colors.CornflowerBlue),
-                    "DEBUG" => new SolidColorBrush(Colors.ForestGreen),
-                    "TRACE" => new SolidColorBrush(Colors.Gray),
-                    "CRITICAL" => new SolidColorBrush(Colors.DarkRed),
-                    "VERBOSE" => new SolidColorBrush(Colors.LightGray),
-                    _ => Brushes.Transparent // Or some default brush
-                };
-            }
-            return Brushes.Transparent; // Default if value is not a string or is null
-        }
+	public class LogLevelToBrushConverter : IValueConverter
+	{
 
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
-            //ConvertBack not needed for this use case.
-            throw new NotImplementedException();
-        }
-    }
+		#region Methods: Public
+
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+			if (value is string levelString) {
+				return levelString.ToUpperInvariant() switch {
+					"ERROR" => new SolidColorBrush(Colors.Red),
+					"WARNING" => new SolidColorBrush(Colors.Orange),
+					"INFO" => new SolidColorBrush(Colors.CornflowerBlue),
+					"DEBUG" => new SolidColorBrush(Colors.ForestGreen),
+					"TRACE" => new SolidColorBrush(Colors.Gray),
+					"CRITICAL" => new SolidColorBrush(Colors.DarkRed),
+					"VERBOSE" => new SolidColorBrush(Colors.LightGray),
+					_ => Brushes.Transparent // Or some default brush
+				};
+			}
+			return Brushes.Transparent; // Default if value is not a string or is null
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+			//ConvertBack not needed for this use case.
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
 }
