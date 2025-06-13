72518496c584b9464c4d086500ff6511e44668ba
OG <o.gojayev2000@gmail.com>
2025-06-13 13:37:34 +0200

    Refactor `TabToVisibilityConverter`: reorganize using directives, structure code with regions for clarity, and improve formatting consistency.

--- Converters/TabToVisibilityConverter.cs
+++ Converters/TabToVisibilityConverter.cs
@@ -1,27 +1,36 @@
 namespace Log_Parser_App.Converters
 {
-    using System;
-    using System.Globalization;
-    using Avalonia.Data.Converters;
-    using Log_Parser_App.Models;
+using System;
+using System.Globalization;
+using Avalonia.Data.Converters;
+using Log_Parser_App.Models;
 
+	#region Class: TabToVisibilityConverter
 
-    public class TabToVisibilityConverter : IValueConverter
-    {
-        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
-            if (value is not TabViewModel tabViewModel || parameter is not string mode) {
-                return false; // Or AvaloniaProperty.UnsetValue if more appropriate
-            }
+	public class TabToVisibilityConverter : IValueConverter
+	{
 
-            return mode switch {
-                "Standard" => tabViewModel.LogType == LogFormatType.Standard,
-                "IIS" => tabViewModel.LogType == LogFormatType.IIS,
-                _ => false
-            };
-        }
+		#region Methods: Public
 
-        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
-            throw new NotImplementedException();
-        }
-    }
+		public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture) {
+			if (value is not TabViewModel tabViewModel || parameter is not string mode) {
+				return false; // Or AvaloniaProperty.UnsetValue if more appropriate
+			}
+			return mode switch {
+				"Standard" => tabViewModel.LogType == LogFormatType.Standard,
+				"IIS" => tabViewModel.LogType == LogFormatType.IIS,
+				_ => false
+			};
+		}
+
+		public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture) {
+			throw new NotImplementedException();
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
 }
