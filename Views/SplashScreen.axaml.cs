0d8e46492cd9f0baa6ba5816b863e0a739191842
OG <o.gojayev2000@gmail.com>
2025-06-13 13:29:54 +0200

    Refactor `SplashScreen.axaml.cs`: reorganize code with regions for clarity, improve formatting consistency, and enhance code structure for better maintainability.

--- Views/SplashScreen.axaml.cs
+++ Views/SplashScreen.axaml.cs
@@ -1,43 +1,59 @@
-using System.Reflection;
-using System.Threading.Tasks;
-using Avalonia.Controls;
-using Avalonia.Markup.Xaml;
-
 namespace Log_Parser_App.Views
 {
-    public partial class SplashScreen : Window
-    {
-        private readonly TextBlock? _statusTextBlock;
-        private readonly TextBlock? _versionTextBlock;
+	using System.Reflection;
+	using Avalonia.Controls;
+	using Avalonia.Markup.Xaml;
 
-        public SplashScreen()
-        {
-            InitializeComponent();
-            _statusTextBlock = this.FindControl<TextBlock>("StatusTextBlock");
-            _versionTextBlock = this.FindControl<TextBlock>("VersionTextBlock");
-            SetVersionNumber();
-        }
+	#region Class: SplashScreen
 
-        private void InitializeComponent()
-        {
-            AvaloniaXamlLoader.Load(this);
-        }
+	public partial class SplashScreen : Window
+	{
 
-        public void UpdateStatus(string status)
-        {
-            if (_statusTextBlock != null)
-            {
-                _statusTextBlock.Text = status;
-            }
-        }
+		#region Fields: Private
 
-        private void SetVersionNumber()
-        {
-            var version = Assembly.GetExecutingAssembly().GetName().Version;
-            if (version != null && _versionTextBlock != null)
-            {
-                _versionTextBlock.Text = $"v{version.Major}.{version.Minor}.{version.Build}";
-            }
-        }
-    }
-} 
+		private readonly TextBlock? _statusTextBlock;
+		private readonly TextBlock? _versionTextBlock;
+
+		#endregion
+
+		#region Constructors: Public
+
+		public SplashScreen() {
+			InitializeComponent();
+			_statusTextBlock = this.FindControl<TextBlock>("StatusTextBlock");
+			_versionTextBlock = this.FindControl<TextBlock>("VersionTextBlock");
+			SetVersionNumber();
+		}
+
+		#endregion
+
+		#region Methods: Private
+
+		private void InitializeComponent() {
+			AvaloniaXamlLoader.Load(this);
+		}
+
+		private void SetVersionNumber() {
+			var version = Assembly.GetExecutingAssembly().GetName().Version;
+			if (version != null && _versionTextBlock != null) {
+				_versionTextBlock.Text = $"v{version.Major}.{version.Minor}.{version.Build}";
+			}
+		}
+
+		#endregion
+
+		#region Methods: Public
+
+		public void UpdateStatus(string status) {
+			if (_statusTextBlock != null) {
+				_statusTextBlock.Text = status;
+			}
+		}
+
+		#endregion
+
+	}
+
+	#endregion
+
+}
